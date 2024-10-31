129ce1f9cbfde9f023c744067c946a3e7838f99e
Elias Tuhola <elias.tuhola@gmail.com>
2024-10-31 11:21:24 +0200

    Setup viikko1/nhl statistics

---
+++ viikko1/nhl-statistics-1/src/index.py
@@ -0,0 +1,21 @@
+from player_reader import PlayerReader
+from statistics_service import StatisticsService
+
+url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+
+def main():
+    stats = StatisticsService(reader=PlayerReader(url))
+    philadelphia_flyers_players = stats.team("PHI")
+    top_scorers = stats.top(10)
+
+    print("Philadelphia Flyers:")
+    for player in philadelphia_flyers_players:
+        print(player)
+
+    print("Top point getters:")
+    for player in top_scorers:
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
