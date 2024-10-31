b7d1d09d853c21344e097390544d5112172c7364
Elias Tuhola <elias.tuhola@gmail.com>
2024-10-31 12:06:56 +0200

    Added eq method for Player class

--- viikko1/nhl-statistics-1/src/player.py
+++ viikko1/nhl-statistics-1/src/player.py
@@ -11,3 +11,6 @@ class Player:
 
     def __str__(self):
         return f"{self.name} {self.team} {self.goals} + {self.assists} = {self.points}"
+    
+    def __eq__(self, other: "Player") -> bool:
+        return self.name == other.name and self.team == other.team and self.goals == other.goals and self.assists == other.assists
