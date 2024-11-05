a972bc589ccb86372c7838d10bdd2417fe14baf7
Elias Tuhola <elias.tuhola@gmail.com>
2024-11-05 11:02:35 +0200

    Parsing toml file

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,3 +1,4 @@
+import tomli
 from urllib import request
 from project import Project
 
@@ -9,7 +10,15 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+
+        toml_dict = tomli.loads(content)
+        poetry = toml_dict['tool']['poetry']
+        name = poetry['name']
+        description = poetry['description']
+        license = poetry['license']
+        authors = poetry['authors']
+        dependencies = poetry['dependencies']
+        dev_dependencies = poetry['group']['dev']['dependencies']
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, description, license, authors, dependencies, dev_dependencies)
