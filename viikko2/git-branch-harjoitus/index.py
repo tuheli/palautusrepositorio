b5c7e80b3113ebed9c786c25107d2dcd24c8e66b
Elias Tuhola <elias.tuhola@gmail.com>
2024-11-05 13:37:42 +0200

    Fixed merge conflict

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -3,11 +3,7 @@ from summa import summa
 from erotus import erotus
 from tulo import tulo
 
-<<<<<<< HEAD
-logger("aloitetaan ohjelma")
-=======
 logger("aloitetaan! ohjelma") # muutos mainissa
->>>>>>> d195904 (Modified index in local copy 2)
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
