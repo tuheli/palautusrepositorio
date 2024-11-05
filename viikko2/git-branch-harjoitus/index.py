1eaf3f4a4cf8aeb246b158e9e5b374cca093af48
Elias Tuhola <elias.tuhola@gmail.com>
2024-11-05 13:11:52 +0200

    Using tulo in index.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,14 +1,15 @@
-# tehdään alussa importit
-
 from logger import logger
 from summa import summa
 from erotus import erotus
+from tulo import tulo
 
-logger("aloitetaan ohjelma") # muutos mainissa
+logger("aloitetaan ohjelma")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"{x} + {y} = {summa(x, y)}") 
+print(f"{x} - {y} = {erotus(x, y)}") 
+print(f"{x} * {y} = {tulo(x, y)}") 
 
-logger("lopetetaan ohjelma")
+logger("lopetetaan")
+print("goodbye!")
