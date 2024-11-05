66b9ecc9b2da22b588a17eeb3dd8f3b6578c8c6b
Elias Tuhola <elias.tuhola@gmail.com>
2024-11-05 11:03:10 +0200

    Updated Project __str__ format

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,24 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        return f'\n- {"\n- ".join(dependencies)}' if len(dependencies) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
+            f"\nLicense: {self.license or '-'}"
+            f"\n"
+            f"\nAuthors: {self.authors or '-'}"
+            f"\n"
             f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
+            f"\n"
             f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
         )
